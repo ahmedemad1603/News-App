7f9eae132f20d66d3fb93dcb16a207d2499b4c90
Ahmed Atalla <ahmed162003@gmail.com>
2024-09-25 11:32:11 +0300

    + Using APIs

---
+++ lib/api/api_manager.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:news_app/model/NewsResponse/NewsResponse.dart';
+import '../model/SourcesResponse/SourcesResponse.dart';
+
+class ApiManager
+{
+  static const String baseUrl = "newsapi.org";
+  static const String apiKey = "ed0044364aa24c2fada3a7451da11bb5";
+
+
+  static Future<SourcesResponse> getSources(String categoryId) async
+  {
+    // ?=
+    Uri sourcesApiUrl = Uri.https(baseUrl, "/v2/top-headlines/sources", {
+      "apiKey": apiKey,
+      "category": categoryId
+    });
+
+    http.Response response = await http.get(sourcesApiUrl);
+    Map<String, dynamic> json = jsonDecode(response.body);
+    SourcesResponse sourcesResponse = SourcesResponse.fromJson(json);
+    return sourcesResponse;
+  }
+
+  static Future<NewsResponse> getNews(String sourceId) async
+  {
+    // ?=&
+    Uri newsUrl = Uri.http(baseUrl, "/v2/everything", {
+      "apiKey": apiKey,
+      "sources": sourceId
+    });
+
+    http.Response response = await http.get(newsUrl);
+    Map<String, dynamic> json = jsonDecode(response.body);
+    NewsResponse newsResponse = NewsResponse.fromJson(json);
+    return newsResponse;
+  }
+
+}
--- lib/ui/category_details/category_details.dart
+++ lib/ui/category_details/category_details.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/api/api_manager.dart';
 import 'package:news_app/ui/category_details/widgets/news_list.dart';
 import 'package:news_app/ui/category_details/widgets/source_tab.dart';
+import '../../model/SourcesResponse/Source.dart';
 
-class CategoryDetails extends StatelessWidget
+class CategoryDetails extends StatefulWidget
 {
   String categoryId;
 
   CategoryDetails({super.key,required this.categoryId});
 
-  List<String> sourceNames = [
-    "BBC News",
-    "Al-Jazeera News",
-    "MBC News",
-    "CBC News",
-    "CNN News",
-  ];
+  @override
+  State<CategoryDetails> createState() => _CategoryDetailsState();
+}
 
+class _CategoryDetailsState extends State<CategoryDetails> {
   @override
   Widget build(BuildContext context)
   {
-    return DefaultTabController(
-      length: sourceNames.length,
-      child: Column(
-        children: [
-          TabBar(
-            padding: const EdgeInsets.all(16),
-            isScrollable: true,
-            labelColor: Colors.white,
-            unselectedLabelColor: Theme.of(context).colorScheme.primary,
-            indicator: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(color: Theme.of(context).colorScheme.primary),
-              color: Theme.of(context).colorScheme.primary
+    return FutureBuilder(
+        future: ApiManager.getSources(widget.categoryId),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting)
+            {
+              return const Center(child: CircularProgressIndicator());
+            }
+          if(snapshot.hasError || snapshot.data?.status == "error")
+            {
+              return Column(
+                children: [
+                  Text(snapshot.data?.message??snapshot.error.toString()),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+
+                        });
+                      },
+                      child: const Text("Try Again")
+                  )
+                ],
+              );
+            }
+          List<Source> sources = snapshot.data?.sources??[];
+          return DefaultTabController(
+            length: sources.length,
+            child: Column(
+              children: [
+                TabBar(
+                    padding: const EdgeInsets.all(16),
+                    isScrollable: true,
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Theme.of(context).colorScheme.primary,
+                    indicator: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(color: Theme.of(context).colorScheme.primary),
+                        color: Theme.of(context).colorScheme.primary
+                    ),
+                    tabs: sources.map((source) => SourceTabWidget(source: source)).toList()
+                ),
+                NewsListWidget(sourceNames: sources)
+              ],
             ),
-            tabs: sourceNames.map((source) => SourceTabWidget(source: source)).toList()
-          ),
-          NewsListWidget(sourceNames: sourceNames)
-        ],
-      ),
+          );
+        },
     );
   }
 }
